2d3a64fa9c5bd3da899e0386051acb5163261a84
yfm <1784015068@qq.com>
2023-03-26 23:47:21 +0800

    2023/3/26

---
+++ src/utils/request.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+// import store from '@/store'
+// import { getToken } from '@/utils/auth'
+
+const service = axios.create({
+  // 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
+  // 如果执行 npm run build 值为 /prod-api  没关系  运维应该在上线的时候 给你配置上 /prod-api的代理
+  baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础的基础地址
+  timeout: 5000 // 定义5秒超时
+}) // 创建一个axios的实例
+
+// // request interceptor
+// service.interceptors.request.use(
+//   config => {
+//     // do something before request is sent
+
+//     if (store.getters.token) {
+//       // let each request carry token
+//       // ['X-Token'] is a custom headers key
+//       // please modify it according to the actual situation
+//       config.headers['X-Token'] = getToken()
+//     }
+//     return config
+//   },
+//   error => {
+//     // do something with request error
+//     console.log(error) // for debug
+//     return Promise.reject(error)
+//   }
+// )
+
+// 响应拦截器
+service.interceptors.response.use(response => {
+  const { success, message, data } = response.data// axios默认加了一层data
+  //   要根据success的成功与否判断是否有业务逻上的错误
+  if (success) {
+    return data
+  } else {
+    // 业务已经错误了 还能进then ? 不能 ！ 应该进catch
+    Message.error(message) // 提示错误消息
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 提示错误信息
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+})
+
+export default service
---
+++ src/utils/validate.js
@@ -0,0 +1,27 @@
+/**
+ * Created by PanJiaChen on 16/11/18.
+ */
+
+/**
+ * @param {string} path
+ * @returns {Boolean}
+ */
+export function isExternal(path) {
+  return /^(https?:|mailto:|tel:)/.test(path)
+}
+
+/**
+ * @param {string} str
+ * @returns {Boolean}
+ */
+export function validUsername(str) {
+  const valid_map = ['admin', 'editor']
+  return valid_map.indexOf(str.trim()) >= 0
+}
+
+/**
+ * 添加一个校验手机号验证方法
+ * **/
+export function validMobile(str) {
+  return /^1[3-9]\d{9}$/.test(str) // 校验手机号
+}
